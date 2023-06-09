8f3dd7e35e04ff3bcee470b9f2ca9cb351f8a7b4
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-06-09 03:38:19 +0100

    Experimenting with try except statements, trying to catch errors and display prompts to user based on trigger.

--- calc.py
+++ calc.py
@@ -1,64 +1,71 @@
 # Simple Calculator Input function
+print("Use 'q' as an operator to exit calculator")
+input("Press return to continue...")
+print()
 def calculatorFunction():
-    num1 = float(input("Enter the number: "))
+    inUseOperators = ["+", "-", "/", "*", "q"]
+    num1 = input("Enter the number: ")
     operator = input("Enter an operator ( - + / *): ")
-    num2 = float(input("Enter the number: "))
-
+    num2 = input("Enter the number: ")
+    num = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+      
     if operator == "+":
-        result = float(round(num1 + num2, 2))
+        result = round(float(num1) + float(num2), 2)
         print(result)
+        calculatorFunction()
     elif operator == "-":
-        result = float(round(num1 - num2, 2))
+        result = round(float(num1) - float(num2), 2)
         print(result)
+        calculatorFunction()
     elif operator == "/":
-        result = float(round(num1 / num2, 2))
+        result = round(float(num1) / float(num2), 2)
         print(result)
+        calculatorFunction()
     elif operator == "*":
-        result = float(round(num1 * num2, 2))
+        result = round(float(num1) * float(num2), 2)
         print(result)
-    else:
-        print("Invalid operator")
+        calculatorFunction()
+    elif operator == "q":
+        print("exit")
+    
+    
+    
+    # Experimenting with try and catch
+
+    # elif isinstance(num, float) == (num1 and num2):
+    #     try:
+    #         print("Input field has to be a number")
+    #         calculatorFunction()
+    #     except:
+    #         print(num1, num2)
+    
+    # if (num1 and num2).isspace == True:
+    #     try:
+    #         print("Input field cannot be empty")
+    #         calculatorFunction()
+    #     except:
+    #         print(num1, num2)
+  
+    elif operator != inUseOperators:
+        try:
+            (operator in inUseOperators) == False
+        except:
+            print("Invalid operator")
+            calculatorFunction()
+        else:
+            print(f'Operator state: {operator}')
+    # elif operator == inUseOperators:
+    #     print(f'Operator state: {operator}')
+    # e
+            
+            # print("Invalid value for number")
+            # calculatorFunction()
+
+    
+    #     # else:
+    #     #     operator == "break"
+    #     #     return None
+        
     
 
 calculatorFunction()
-
-# ----------------------------------------------------------------
-
-# Calculator Function v2.0
-# Trying to create a state where contiuous calculations can be
-# performed within one session. How to use while & for loops to
-# achieve this?
-
-# def calculatorFunction():
-#     num1 = float(input("Enter the number: "))
-#     operator = input("Enter an operator ( - + / *): ")
-#     num2 = float(input("Enter the number: "))
-#     i = 0
-#     while i > 1:
-#         if operator == "+":
-#             result = float(round(num1 + num2, 2))
-#             print(result)
-#             return result
-#         elif operator == "-":
-#             result = float(round(num1 - num2, 2))
-#             print(result)
-#             return result
-#         elif operator == "/":
-#             result = float(round(num1 / num2, 2))
-#             print(result)
-#             return result
-#         elif operator == "*":
-#             result = float(round(num1 * num2, 2))
-#             print(result)
-#             return result
-#         elif operator == "!":
-#             i +=1
-#             return i
-#         else:
-#             print("Invalid operator")
-#         print(result)
-#         session = result
-#         break
-
-
-# calculatorFunction()
