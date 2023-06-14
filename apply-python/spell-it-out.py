8de15933daade08461dcf9aa496be4cab56814b7
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-06-14 03:11:03 +0100

    Refactored spell-it-out.py and added more comments.

--- apply-python/spell-it-out.py
+++ apply-python/spell-it-out.py
@@ -1,15 +1,20 @@
-text = list(input("Enter statement to be spelled out: "))
 
-def spell(text):
-    # space = [" "]
+def spell():
+    text = input("Enter statement to be spelled out: ")
+
     for item in text:
-        # str_len = len(item)
+        # 1. Using for loop and conditions statements:
         if item.isspace() == True:
             print("SPACE")
-        elif (isinstance(item, str)) == True:
+        else:
             print(item)
+
+        # 2. Using replace() function to replace spaces in text:
+        # space_replace = item.replace(" ", "SPACE")
+        # print(space_replace)
+
     print()
     print("How's my spelling?")
 
 
-spell(text)
+spell()
